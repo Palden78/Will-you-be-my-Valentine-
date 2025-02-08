d49e932e3871ff23707af2838688c13f8b735972
Palden78 <palden6234@gmail.com>
2025-02-08 20:11:31 +0800

    Edited the images

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const messages = [
     "Are you sure?",
     "Really sure??",
-    "Are you positive?",
+    "You better watch out for fat orang cat",
     "Pookie please...",
-    "Just think about it!",
-    "If you say no, I will be really sad...",
-    "I will be very sad...",
-    "I will be very very very sad...",
-    "Ok fine, I will stop asking...",
+    "Meoww",
+    "If you say no, I will be really sad... :(",
+    "Meoww :(",
+    "MEOWW :(",
+    "Ok fine, I will stop asking :3",
     "Just kidding, say yes please! ❤️"
 ];
 
